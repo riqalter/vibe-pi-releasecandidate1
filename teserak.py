415b029d674f10e81f2ab24f3bac8d40e06fd75a
riqtiny <200603015@student.umri.ac.id>
2025-07-15 22:47:42 +0700

    remove unnecessary file
    Signed-off-by: riqtiny <200603015@student.umri.ac.id>

--- teserak.py
+++
@@ -1,3 +0,0 @@
-import pytesseract
-print(pytesseract.get_tesseract_version())
-print(pytesseract.get_languages())
--- test.py
+++
@@ -1,7 +0,0 @@
-import uuid
-
-def generate_uuid():
-    return str(uuid.uuid4())
-
-# test
-print(generate_uuid())
